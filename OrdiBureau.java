757e7acbe0936dc7fb273f62836d77442e409051
Idrissa Muwanga <80053968+IdrissaMuwanga@users.noreply.github.com>
2022-03-30 15:07:54 +0200

    Update OrdiBureau.java

--- OrdiBureau.java
+++ OrdiBureau.java
@@ -2,33 +2,44 @@ public class OrdiBureau extends Ordinateur{
 private String carteGraphique;
 private String chipset;
 private String clavier;
+  
 //Constructeur
+  
 public OrdiBureau(String reference, String processeur, String ram, String disqueDur, String systemExploitation,int prix,String carteGraphique,String chipset,String clavier) {
 super(reference, processeur, ram, disqueDur, systemExploitation, prix);
 this.carteGraphique=carteGraphique;
 this.chipset=chipset;
 this.clavier=clavier;
 }
-/Getter et Setter
+  
+//Getter et Setter
+  
 public String getCarteGraphique() {
 return carteGraphique;
 }
+  
 public void setCarteGraphique(String carteGraphique) {
 this.carteGraphique = carteGraphique;
 }
+  
 public String getChipset() {
 return chipset;
 }
+  
 public void setChipset(String chipset) {
 this.chipset = chipset;
 }
+  
 public String getClavier() {
 return clavier;
 }
+  
 public void setClavier(String clavier) {
 this.clavier = clavier;
 }
+  
 //methode toString()
+  
 public String toString() {
 return super.toString()+" [carteGraphique= " + carteGraphique + ", chipset= " + chipset + ", clavier= " + clavier + " ] ";
 }
